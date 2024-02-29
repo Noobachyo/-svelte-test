b009f49aa0ef577e1f4d4b4744fe977b1610a17f
Noobachyo <serezha_dubinin_2004@mail.ru>
2024-02-29 21:30:06 +0700

    final

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     <a href="/" class="nav-logo" title="Back to Homepage">Сергей Дубинин</a>
     <div class="nav-links">
       {#each nav as link}
-      <a href={link.href} class="link">{link.title}</a>
+      {#if link.isShow} <a href={link.href} class="link">{link.title}</a> {/if}
       {/each}
     </div>
  </div>
@@ -15,33 +15,33 @@
 </div>
 
 <script>
- const nav = [
-{
+const nav = [
+ {
  title: 'Обо мне',
- href: '/about'
-},
-
-{
+ href: '/about',
+ isShow: true,
+ },
+ {
  title: 'Блог',
- href: '/blog'
-},
-
-{
-title: 'Портфолио',
-href: '/project'
-},
-
-{
+ href: '/blog',
+ isShow: true,
+ },
+ {
+ title: 'Портфолио',
+ href: '/project',
+ isShow: true,
+ },
+ {
  title: 'Контакты',
- href: '/contact'
-},
-
-{
+ href: '/contact',
+ isShow: true,
+ },
+ {
  title: 'temp',
- href: '/temp'
-}
-
-]
+ href: '/temp',
+ isShow: true,
+ },
+];
 </script>
 
 <style>
